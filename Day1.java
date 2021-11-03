4694933a780ecf9ed92215a4afb8b280b4b68592
Uttamkumar Manani <uttammanani@gmail.com>
2021-11-03 10:37:56 +0530

    Added solution of Day-1

---
+++ Day1.java
@@ -0,0 +1,12 @@
+//import java.io.*;
+import java.util.*;
+
+public class Day1 {
+
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+        String s = s1.nextLine();
+        System.out.println("Hello, World.");
+        System.out.println(s);
+    }
+}
