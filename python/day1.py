cea682dae04c8c247bf1cf384078a0e898189607
Uttamkumar Manani <uttammanani@gmail.com>
2021-11-13 23:32:39 +0530

    Added solution of Day-1

---
+++ python/day1.py
@@ -0,0 +1,19 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+int1 = int(input())
+double1 = float(input())
+string1 = input()
+
+print(int1)
+print(double1)
+print(string1)
+
+int1 += i
+double1 += d
+string1 += s
+
+print(int1)
+print(double1)
+print(string1)
